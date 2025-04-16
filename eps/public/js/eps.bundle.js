1c1dd89eff09ac866d9eb38548ad8289d594bde8
sokumon <sohamkulkarns9@gmail.com>
2025-04-16 21:15:25 +0530

    fix: setup event listners in desk

--- eps/public/js/eps.bundle.js
+++ eps/public/js/eps.bundle.js
@@ -2,3 +2,4 @@ import "./review.html";
 import "./review.js";
 import "../../eps/page/user_profile/user_profile_controller.js"
 import "./utils/energy_point_utils.js"
+import "./energy_point_alert.js"
