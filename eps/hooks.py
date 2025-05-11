1406b60bd9cc991fc75f72852e3c99f0df54d4a7
sokumon <sohamkulkarns9@gmail.com>
2025-05-11 12:15:39 +0530

    fix: add email header

--- eps/hooks.py
+++ eps/hooks.py
@@ -43,9 +43,6 @@ app_license = "mit"
 # page_js = {"page" : "public/js/file.js"}
 
 # include js in doctype views
-doctype_js = {
-	"*": "public/js/review.js",
-}
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
 # doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
